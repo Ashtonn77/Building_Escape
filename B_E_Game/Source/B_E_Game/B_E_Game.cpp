f110de78b521d4e5cdb3712cd1aa3e7f3d6101fe
Ashton Naidoo <ashtonrbay@gmail.com>
2020-10-20 22:53:20 +0200

    init commit

---
+++ B_E_Game/Source/B_E_Game/B_E_Game.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "B_E_Game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, B_E_Game, "B_E_Game" );
---
+++ B_E_Game/Source/B_E_Game/B_E_GameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "B_E_GameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class B_E_GAME_API AB_E_GameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
